6bf5e661db9137291a603e9f2476f302add8b42c
ryx <1596342366@qq.com>
2019-06-11 15:42:51 +0800

    ryx

--- laravel/app/Modules/Base/Http/Controllers/Member_manageController.php
+++ laravel/app/Modules/Base/Http/Controllers/Member_manageController.php
@@ -8,8 +8,45 @@
 
 namespace App\Modules\Base\Http\Controllers;
 
+use DB;
+use App\Http\Controllers\Controller;
 
-class Member_manageController
+
+class Member_manageController extends Controller
 {
+		public function index()
+   {
+       echo 123;
+   }
+
+   public function admin_list()//管理员列表
+   {
+   		
+   		return view('base::index.admin_list');
+   }
+
+   public function admin_role()//角色管理
+   {
+   		return view();
+   }
+
+   public function admin_cate()//分配角色
+   {
+   		return view();
+   }
+
+   public function admin_rule()//分配权限
+   {
+   		return view();
+   }
+
+
+
+
+
+
+
+
+
 
 }
--- laravel/app/Modules/Base/Routes/web.php
+++ laravel/app/Modules/Base/Routes/web.php
@@ -24,4 +24,36 @@ Route::group(['prefix' => 'base'], function () {
 
     Route::get('/alist','AdminController@alist');
 
+    
+	Route::any('moneyoff_acticity','Active_manageController@moneyoff_acticity');
+	Route::any('discount_coupon','Active_manageController@discount_coupon');
+	Route::any('full_give','Active_manageController@full_give');
+	
+	Route::any('add','Brand_manageController@add');
+	Route::any('lists','Brand_manageController@lists');
+	Route::any('attribute_add','Commodity_manageController@attribute_add');
+	Route::any('attribute_list','Commodity_manageController@attribute_list');
+	Route::any('attribute_list_attribute_manage','Commodity_manageController@attribute_list_attribute_manage');
+	Route::any('attribute_list_attribute_manage_lists','Commodity_manageController@attribute_list_attribute_manage_lists');
+	Route::any('attribute_list_attribute_manage_add','Commodity_manageController@attribute_list_attribute_manage_add');
+	Route::any('add','Goodscategory_manageController@add');
+	Route::any('lists','Goodscategory_manageController@lists');
+	Route::any('index','IndexController@index');
+	Route::any('lists','Order_manageController@lists');
+	Route::any('state','Order_manageController@state');
+	Route::any('list_compile','Order_manageController@list_compile');
+	Route::any('list_compile_state_amend','Order_manageController@list_compile_state_amend');
+	Route::any('list_compile_user_amend','Order_manageController@list_compile_user_amend');
+	Route::any('list_compile_commodity_amend','Order_manageController@list_compile_commodity_amend');
+	Route::any('state_add','Order_manageController@state_add');
+	Route::any('state_list','Order_manageController@state_list');
+	Route::any('comment_audit','Service_manageController@comment_audit');
+	Route::any('comment_reply','Service_manageController@comment_reply');
+	Route::any('opinion_list','Service_manageController@opinion_list');
+	Route::any('opinion_reply','Service_manageController@opinion_reply');
+	Route::any('warehouse_add','Store_manageController@warehouse_add');
+	Route::any('warehouse_list','Store_manageController@warehouse_list');
+
+	// Route::any('qwertyuiop','Member_manageController@index');
+
 });
